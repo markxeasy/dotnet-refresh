b53814d76334349199e1304556b2ef0050b9925f
Marc <marc.moser@dracodex.com>
2020-05-24 23:15:22 +0200

    Moved the project to a K8S solution

--- dotnet-refresh/Startup.cs
+++ dotnet-refresh/Startup.cs
@@ -55,7 +55,7 @@ namespace dotnet_refresh
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
